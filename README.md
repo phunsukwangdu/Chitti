2afe3795c3ad5647273798d84d9f6d44159dc8bc
phunsukwangdu <susmit9370@gmail.com>
2017-04-10 09:19:28 +0530

    adds stt + tts feature to chatbot making it virtual assistant

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Chitti
-AI chatbot based on AIML.<br>
-To start chitti chatbot : `python chitti.py`
+AI Virtual assistant based on AIML.<br>
+To start Chitti virtual assistant : `python chitti.py`<br>
+To exit Chitt : just say "exit"<br>
+It is a attempt to make a simple personal assistant that takes speech as input and says the processed data based on AIML.AIML is simple pattern matching and does not implement any sort of machine learning.In near future Chitti will be capable of doing simple tasks such as opening webbrowser and various small activities.Futher instructions and enhancements will be added soon :P.
--- chitti.py
+++ chitti.py
@@ -1,4 +1,6 @@
 import aiml
+import speech_recognition as sr
+import pyttsx
 import os
 
 kernel = aiml.Kernel()
@@ -9,6 +11,36 @@ else:
     kernel.bootstrap(learnFiles = "std-startup.xml", commands = "load aiml b")
     kernel.saveBrain("chitti_brain.brn")
 
-# kernel now ready for use
+def vocalize(audioStr):
+    print audioStr
+    engine = pyttsx.init()
+    engine.say(audioStr)
+    engine.runAndWait()
+
+def recordAudio():
+    # Record Audio
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Say something!")
+        audio = r.listen(source)
+ 
+    # Speech recognition using Google Speech Recognition
+    data = ""
+    try:
+        # Uses the default API key
+        # To use another API key: `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+        data = r.recognize_google(audio)
+        print("You said: " + data)
+    except sr.UnknownValueError:
+        print("Google Speech Recognition could not understand audio")
+    except sr.RequestError as e:
+        print("Could not request results from Google Speech Recognition service; {0}".format(e))
+    return data
+
 while True:
-    print kernel.respond(raw_input("Chitti >> "))
+    data = recordAudio()
+    if "exit" in data:
+        break
+    if len(data)!=0:
+        ProData = kernel.respond(data)
+        vocalize(ProData)
